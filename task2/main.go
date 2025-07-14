96573e7c493c119236610d21689c4540eed91386
zuekveer <simetramonk@gmail.com>
2025-07-14 22:00:38 +0300

    task2

--- task2/main.go
+++ task2/main.go
@@ -14,3 +14,43 @@ func (c *SafeCounter) Inc()
 func (c *SafeCounter) Value() int
 ```
 */
+
+package task2
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeCounter struct {
+	value int
+	mu    sync.RWMutex
+}
+
+func (c *SafeCounter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *SafeCounter) Value() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.value
+}
+
+func main() {
+	counter := SafeCounter{}
+
+	var wg sync.WaitGroup
+	for range 1000 {
+		wg.Add(1)
+		go func() {
+			counter.Inc()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counter.Value())
+}
