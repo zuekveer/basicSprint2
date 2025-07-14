e793c8533c4733bdc606db40fc42e84770aac1e9
zuekveer <simetramonk@gmail.com>
2025-07-14 21:59:33 +0300

    tasл2

--- task3/main.go
+++ task3/main.go
@@ -7,3 +7,43 @@ func WaitAny(a, b <-chan string) string {
 	// ваш код
 }
 */
+
+package task3
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeCounter struct {
+	value int
+	mu    sync.RWMutex
+}
+
+func (c *SafeCounter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *SafeCounter) Value() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.value
+}
+
+func main() {
+	counter := SafeCounter{}
+
+	var wg sync.WaitGroup
+	for range 1000 {
+		wg.Add(1)
+		go func() {
+			counter.Inc()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counter.Value())
+}
