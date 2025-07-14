b8193b2bd61f3d1e97e9707d026acd13dcf01695
zuekveer <simetramonk@gmail.com>
2025-07-14 22:56:09 +0300

    task7

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module basicSprint2
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.23.11
+
+require golang.org/x/exp v0.0.0-20250711185948-6ae5c78190dc
--- task7/main.go
+++ task7/main.go
@@ -4,6 +4,29 @@
 Задача:
 Напиши обобщённую функцию SumSlice, которая суммирует элементы слайса чисел (int, float64, ...):
 func SumSlice[T Number](items []T) T {
-	// 
+	//
 }
 */
+
+package task7
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+func SumSlice[T constraints.Integer | constraints.Float](items []T) T {
+	var sum T
+	for _, v := range items {
+		sum += v
+	}
+	return sum
+}
+func main() {
+	ints := []int{1, 2, 3, 4, 5}
+	fmt.Println(SumSlice(ints)) 
+
+	floats := []float64{1.1, 2.2, 3.3}
+	fmt.Println(SumSlice(floats)) 
+}
