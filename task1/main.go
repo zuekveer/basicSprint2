9969ddda7b6844c03989f9e8f9ebc1ddba68574a
zuekveer <simetramonk@gmail.com>
2025-07-14 21:43:47 +0300

    task1

--- task1/main.go
+++ task1/main.go
@@ -6,8 +6,34 @@
 Напиши функцию `RunNTimesConcurrently`, которая запускает `n` горутин, каждая из которых печатает свой номер, и программа корректно завершается после их завершения.
 
 ```go
-func RunNTimesConcurrently(n int) {
-	// ваш код
-}
+
+	func RunNTimesConcurrently(n int) {
+		// ваш код
+	}
+
 ```
 */
+package task1
+
+import (
+	"fmt"
+	"sync"
+)
+
+func RunNTimesConcurrently(n int) {
+	var wg sync.WaitGroup
+
+	for i := range n {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done() 
+			fmt.Println(num)
+		}(i) 
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	RunNTimesConcurrently(5)
+}
